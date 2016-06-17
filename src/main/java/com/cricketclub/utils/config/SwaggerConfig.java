cff7c39fad34999cbc3fdaa103906cffdc0db545
Ankit Rana <Ankit.Rana@Rightmove.co.uk>
2016-06-17 11:23:26 +0100

    re swagger now shows all the enpoint however throws 403 when you sends data

--- src/main/java/com/cricketclub/utils/config/SwaggerConfig.java
+++ src/main/java/com/cricketclub/utils/config/SwaggerConfig.java
@@ -1,83 +1,86 @@
 package com.cricketclub.utils.config;
 
-import com.mangofactory.swagger.configuration.SpringSwaggerConfig;
-import com.mangofactory.swagger.models.dto.*;
-import com.mangofactory.swagger.models.dto.builder.ApiInfoBuilder;
-import com.mangofactory.swagger.models.dto.builder.OAuthBuilder;
-import com.mangofactory.swagger.plugin.EnableSwagger;
-import com.mangofactory.swagger.plugin.SwaggerSpringMvcPlugin;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.context.request.async.DeferredResult;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.AuthorizationCodeGrant;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.GrantType;
+import springfox.documentation.service.OAuth;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.TokenEndpoint;
+import springfox.documentation.service.TokenRequestEndpoint;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.security.Principal;
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
-import static com.mangofactory.swagger.models.alternates.Alternates.newRule;
+import static com.google.common.collect.Lists.newArrayList;
 
 @Configuration
-@EnableSwagger
-@EnableAutoConfiguration
+@EnableSwagger2
 public class SwaggerConfig {
+
+	private static final String securitySchemaOAuth2 = "oauth2schema";
+	private static final String authorizationScopeGlobal = "global";
+	private static final String authorizationScopeGlobalDesc ="accessEverything";
 
 	@Value("${oauth.authzEndpoint}")
 	private String swaggerOAuthUrl;
-
-	private SpringSwaggerConfig springSwaggerConfig;
-
-	@Autowired
-	public void setSpringSwaggerConfig(SpringSwaggerConfig springSwaggerConfig) {
-		this.springSwaggerConfig = springSwaggerConfig;
-	}
+	@Value("${oauth.clientId}")
+	private String oAuthClientId;
+	@Value("${oauth.clientSecret}")
+	private String oAuthClientSecret;
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("Cricket Club Management API")
                 .description("This service provides a JSON API for managing cricket club related data")
                 .termsOfServiceUrl("http://springfox.io")
-                .contact("springfox")
                 .license("Apache License Version 2.0")
                 .licenseUrl("https://github.com/springfox/springfox/blob/master/LICENSE")
                 .build();
     }
 
-    @Bean
-	public SwaggerSpringMvcPlugin customImplementation() {
-		return new SwaggerSpringMvcPlugin(this.springSwaggerConfig)
-				//Root level documentation
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.cricketclub"))
+				.paths(PathSelectors.any())
+				.build()
 				.apiInfo(apiInfo())
 				.useDefaultResponseMessages(false)
-				//Map the specific URL patterns into Swagger
-				.includePatterns("/my")
-				.authorizationTypes(getAuthorizationTypes())
-				.ignoredParameterTypes(OAuth2Authentication.class, Principal.class);
+				.securitySchemes(newArrayList(securitySchema()))
+				.securityContexts(newArrayList(securityContext()));
 	}
 
-	private List<AuthorizationType> getAuthorizationTypes() {
-		List<AuthorizationType> authorizationTypes = new ArrayList<>();
-		List<AuthorizationScope> scopes = new ArrayList<>();
-		scopes.add(new AuthorizationScope("my-resource.read","Read access on the API"));
+	private OAuth securitySchema() {
+		AuthorizationScope authorizationScope = new AuthorizationScope(authorizationScopeGlobal, authorizationScopeGlobal);
+		TokenEndpoint tokenEndpoint = new TokenEndpoint(swaggerOAuthUrl, "access_code");
+		GrantType grantType = new AuthorizationCodeGrant(new TokenRequestEndpoint(swaggerOAuthUrl, oAuthClientId, oAuthClientSecret), tokenEndpoint);
+		return new OAuth(securitySchemaOAuth2, newArrayList(authorizationScope), newArrayList(grantType));
+	}
 
-		List<GrantType> grantTypes = new ArrayList<>();
-		ImplicitGrant implicitGrant = new ImplicitGrant(new LoginEndpoint(swaggerOAuthUrl),"access_code");
-		grantTypes.add(implicitGrant);
-
-		AuthorizationType oauth = new OAuthBuilder()
-				.scopes(scopes)
-				.grantTypes(grantTypes)
+	private SecurityContext securityContext() {
+		return SecurityContext.builder()
+				.securityReferences(defaultAuth())
 				.build();
-		authorizationTypes.add(oauth);
-		return authorizationTypes;
+	}
+
+	private List<SecurityReference> defaultAuth() {
+		AuthorizationScope authorizationScope
+				= new AuthorizationScope(authorizationScopeGlobal, authorizationScopeGlobalDesc);
+		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+		authorizationScopes[0] = authorizationScope;
+		return newArrayList(
+				new SecurityReference(securitySchemaOAuth2, authorizationScopes));
 	}
 
 }
